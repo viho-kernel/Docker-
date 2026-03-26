a2b5f031237e13278596d4ba4cc928085640a72b
viho-kernel <vihari.reddy1802@gmail.com>
2026-03-26 17:34:34 +0530

    latest commit

--- Dockerfiles/Examples/app.py
+++ Dockerfiles/Examples/app.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+PORT = 8080
+
+class HelloHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"Hello World from Docker!")
+
+if __name__ == "__main__":
+    server = HTTPServer(("", PORT), HelloHandler)
+    print(f"Serving on port {PORT}...")
+    server.serve_forever()
