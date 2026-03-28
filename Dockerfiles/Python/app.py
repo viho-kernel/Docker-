2dd3c0b06b9a403bdae801a6aec197f5b37bbca6
viho-kernel <vihari.reddy1802@gmail.com>
2026-03-28 11:19:55 +0530

    latest commit

--- Dockerfiles/Python/app.py
+++ Dockerfiles/Python/app.py
@@ -8,13 +8,13 @@ environment = os.getenv('APP_ENVIRONMENT', 'dev')
 
 @app.route('/')
 def home():
-# Serve different templates based on environment
-if environment == 'dev':
-return render_template('dev.html')
-elif environment == 'qa':
-return render_template('qa.html')
-else:
-return "<h1>Unknown Environment</h1>"
+    # Serve different templates based on environment
+    if environment == 'dev':
+        return render_template('dev.html')
+    elif environment == 'qa':
+        return render_template('qa.html')
+    else:
+        return "<h1>Unknown Environment</h1>"
 
 if __name__ == "__main__":
-app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
